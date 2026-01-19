bc4d6b6c36a59b69bd68462cdbf117a829cad61d
royer <royercfm@gmail.com>
2026-01-18 21:42:23 -0500

    feat: Add favicon and update the main application layout to include it.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'SGCI DRE Huánuco') }}</title>
         
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
+        
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
