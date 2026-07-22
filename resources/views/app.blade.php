1dd34702af92f43822db63edd1b0da767fe49420
royer <royercfm@gmail.com>
2026-07-22 09:51:41 -0500

    feat(branding): actualizar favicon del sistema con el logo oficial de la DRE Huánuco

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,9 @@
         <title inertia>{{ config('app.name', 'SGCI DRE Huánuco') }}</title>
         
         <!-- Favicon -->
-        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-        <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}?v=2">
+        <link rel="shortcut icon" href="{{ asset('images/logo.png') }}?v=2">
+        <link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}?v=2">
         
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
